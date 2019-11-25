d67fc273a16f3f2b3f4c408dcf8a4c467519b297
INDRA/glflores <glflores@indra.es>
2019-11-25 12:21:31 +0100

    refactorización

--- src/main/java/guru/springframework/JokeAppApplication.java
+++ src/main/java/guru/springframework/JokeAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChuckNorrisForActuatorApplication {
+public class JokeAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChuckNorrisForActuatorApplication.class, args);
+        SpringApplication.run(JokeAppApplication.class, args);
     }
 
 }
--- src/main/java/guru/springframework/services/JokesServiceImpl.java
+++ src/main/java/guru/springframework/services/JokesServiceImpl.java
@@ -1,17 +1,14 @@
 package guru.springframework.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokesServiceImpl implements JokesService {
 
-    @Autowired
-    ChuckNorrisQuotes chuckNorrisQuotes;
 
     @Override
     public String getJoke() {
-        return this.chuckNorrisQuotes.getRandomQuote();
+        return ChuckNorrisQuotes.getRandomQuote();
     }
 }
